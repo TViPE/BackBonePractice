6c1705067c0eb84da88e9a46c9e8c5b282a6080d
TRI VI <trivi2710@gmail.com>
2017-04-13 11:08:02 -0400

    Adding and Removing Models
    Feat: Adding and Removing Models
    
    After a collection has been created, models can be added and removed
    using the add() and remove() methods

--- js/collection_00.js
+++ js/collection_00.js
@@ -12,7 +12,23 @@
 
 	var myTodo = new Todo({title: 'Read the whole book', id: 2});
 
-	//pass array of models on collection instantiation
-	var todos = new TodosCollection([myTodo]);
+	var a = new Todo({title: 'Go to Jamaica.'}),
+		b = new Todo({title: 'Go to China'}),
+		c = new Todo({title: 'Go to Disneyland'});
+
+	// pass array of models on collection instantiation
+	var todos = new TodosCollection([a, b]);
+	console.log("Collection size: " + todos.length);
+
+	// add a model to collection
+	todos.add(c);
+	console.log("Collection size: " + todos.length);
+
+	// remove models from collection
+	todos.remove([a,b]);
+	console.log("Collection size: " + todos.length);
+
+	//remove model from collection
+	todos.remove(c);
 	console.log("Collection size: " + todos.length);
 })(jQuery)
